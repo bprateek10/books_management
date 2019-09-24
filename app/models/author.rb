b10fbdd77ac45eebc8756a0ea6bb2f2a17d216fc
Prateek Bhatnagar <bprateek10@yahoo.in>
2019-09-24 16:43:33 +0530

    adding server side validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,10 @@
 class Author < ApplicationRecord
+
+	#Associations
 	has_many :author_books, dependent: :destroy
 	has_many :books, through: :author_books
+
+	#Validations
+	validates :first_name, :last_name, :date_of_birth, presence: true
+	
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+
+	#Associations
 	has_many :author_books, dependent: :destroy
 	has_many :authors, through: :author_books
+
+	#Validations
+	validates :title, presence: true
+	validates :title, uniqueness: true
 end
